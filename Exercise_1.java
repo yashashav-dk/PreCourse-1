0bb30e91edcceb7a4d9fd9664558bd7c6feb6d03
yashashav-dk <ydk61999@gmail.com>
2025-05-12 23:45:01 +0530

    added solutions to 3 exercises in Java

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,35 +1,73 @@
+// Time Complexity : For N elements - pop() takes O(N), push() takes O(N), peek() and isEmpty() is constant
+// Space Complexity : Note we are setting an initial static array of siZe 1000. For N elements - pop() occupies no space, push() occupies O(N) space, peek() and isEmpty() doesn't occupy space
+
+
 class Stack { 
     //Please read sample.java file before starting.
-  //Kindly include Time and Space complexity at top of each file
+    //Kindly include Time and Space complexity at top of each file
     static final int MAX = 1000; 
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        //Check if array a is empty
+        // we check for logical emptyness rather than physical emptyness 
+        if (top == -1) {
+            return true;
+        }
+        else {
+            return false;
+        }
+        
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        // Overflow occurs when we are stacking more than the size of the stack which here is MAX
+        // so we can check if top > MAX - 1 then we run into an overflow
+        if (top >= MAX - 1) {
+            System.out.println("Stack Overflow");
+            return false;
+        } else {
+            top = top + 1;
+            a[top] = x;
+            return true;
+        }
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if (top == -1) {
+            System.out.println("Stack Underflow");
+            return 0;
+        } else {
+            int ele = a[top];
+            a[top] = 0;
+            top = top - 1;
+            return ele;
+        }
+        
     } 
   
     int peek() 
     { 
         //Write your code here
+        if (top > -1) {
+            return a[top];
+        } else {
+            return 0;
+        }
     } 
 } 
   
--- Exercise_2.java
+++ Exercise_2.java
@@ -1,3 +1,7 @@
+// Time Complexity : For N elements; push and pop is O(N) 
+// Space Complexity : For N elements: push and pop is O(N)
+// Note: I have considered that I will be doing operations (pop and push) from one end of the linked list
+
 public class StackAsLinkedList { 
   
     StackNode root; 
@@ -8,38 +12,74 @@ public class StackAsLinkedList {
   
         StackNode(int data) 
         { 
-            //Constructor here 
+            //Constructor here
+            this.data = data;
+            this.next = null;
         } 
     } 
     
 	
     public boolean isEmpty() 
     { 
-        //Write your code here for the condition if stack is empty. 
+        //Write your code here for the condition if stack is empty.
+        if (this.root == null) {
+            return true;
+        } return false;
     } 
   
     public void push(int data) 
     { 
-        //Write code to push data to the stack. 
+        StackNode new_node = new StackNode(data);
+        //Write code to push data to the stack.
+        //Check if the stack is empty
+        if (isEmpty()){
+            //if empty replace the root node
+            this.root = new_node;
+        } //else insert at the end of the root node which is the top of the stack
+        else {
+            // make the new node point to the head node
+            new_node.next = this.root;
+            // later replace the root node with the newly pointed head node which contains new data
+            this.root = new_node;
+        }
     } 
   
     public int pop() 
+    // this is a LIFO / FILO operation. Last in First out / First in Last out
     { 	
 	//If Stack Empty Return 0 and print "Stack Underflow"
-        //Write code to pop the topmost element of stack.
+    if (this.isEmpty()) {
+        System.out.println("Stack Underflow");
+        return 0;
+    }
+    //Write code to pop the topmost element of stack.
+    //Use root to traverse to the end of the list (which is the top of the list).
+    // Note that we are removing and add nodes from one end of the linked list
+    // Store the topmost element
+    int popped_ele = this.root.data;
+    // we are shifting the reference of the root to the next node
+    this.root = this.root.next;
+
+
 	//Also return the popped element 
+    return popped_ele;
     } 
   
     public int peek() 
     { 
         //Write code to just return the topmost element without removing it.
+        if (this.isEmpty()) {
+            return 0;
+        } else {
+            return this.root.data;
+        }
     } 
   
 	//Driver code
     public static void main(String[] args) 
     { 
   
-        StackAsLinkedList sll = new StackAsLinkedList(); 
+        Exercise_2 sll = new Exercise_2(); 
   
         sll.push(10); 
         sll.push(20); 
